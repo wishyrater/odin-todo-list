ba07fbb43bbc04836e5f942f6ad6ce5b50f249b5
Julian Andersen <julian.cesar.andersen@gmail.com>
2024-11-20 15:25:45 +0100

    Create UIRenderer IIFE

---
+++ src/DisplayController.js
@@ -0,0 +1,57 @@
+export const UIRenderer = (function () {
+
+    const clearContainer = (container) => {
+        if (!container) {
+            console.error("Container not found");
+            return;
+        }
+        container.innerHTML = '';
+    };
+
+    const createProjectElement = (project) => {
+        const projectItem = document.createElement("div");
+        projectItem.classList.add("project-item");
+        projectItem.textContent = project.name;
+        return projectItem;
+    };
+
+    const renderProjects = (projects) => {
+        const projectsContainer = document.querySelector(".projects");
+        clearContainer(projectsContainer);
+        projects.projects.forEach((project) => {
+            // create container for this project item
+            const projectItem = createProjectElement(project);
+            projectsContainer.appendChild(projectItem);
+        });
+    };
+
+    const createTaskElement = (task) => {
+        const taskItem = document.createElement("div");
+        taskItem.classList.add("task-item");
+        
+        const taskName = document.createElement("div");
+        taskName.classList.add("task-name");
+        taskName.textContent = task.name;
+
+        const taskDueDate = document.createElement("div");
+        taskDueDate.classList.add("task-due-date");
+        taskDueDate.textContent = task.dueDate;
+
+        const taskStatus = document.createElement("div");
+        taskStatus.classList.add("task-status");
+        taskStatus.textContent = task.status;
+
+        taskItem.append(taskName, taskDueDate, taskStatus);
+    };
+
+    const renderTasks = (project) => {
+        const tasksContainer = document.querySelector(".tasks-container");
+        clearContainer(tasksContainer);
+        project.tasks.forEach((task) => {
+            const taskItem = createTaskElement(task);
+            tasksContainer.appendChild(taskItem);
+        });
+    };
+
+    return { renderProjects, renderTasks };
+})();
