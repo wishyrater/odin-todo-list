8b369df42d21be79cf010f3c628473acb90d5dcd
Julian Andersen <julian.cesar.andersen@gmail.com>
2024-11-23 18:05:05 +0100

    Add project class

---
+++ src/Project.js
@@ -0,0 +1,31 @@
+import Task from './Task';
+export default class Project {
+    constructor(name) {
+        this._name = name;
+        this._tasks = [];
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    set name(newName) {
+        if (newName !== '') this._name = newName;
+    }
+
+    removeTask(index) {
+        if (index > -1 && index < this._tasks.length) {
+            this._tasks.splice(index, 1);
+        } else {
+            throw new Error('Invalid index');
+        }
+    }
+
+    addTask(task) {
+        if (task instanceof Task) this._tasks.push(task);
+    }
+
+    get tasks() {
+        return this._tasks;
+    }
+};
