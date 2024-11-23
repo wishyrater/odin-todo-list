b82adfe6a980adc12991d578d765e7d643b2ab36
Julian Andersen <julian.cesar.andersen@gmail.com>
2024-11-23 17:53:54 +0100

    Add task class

---
+++ src/Task.js
@@ -0,0 +1,32 @@
+export default class Task {
+    constructor(name, dueDate, status) {
+        this._name = name;
+        this._dueDate = dueDate;
+        this._status = status;
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    set name(newName) {
+        if (newName !== '') this._name = newName;
+    }
+
+    get dueDate() {
+        return this._dueDate;
+    }
+
+    set dueDate(newDueDate) {
+        if (newDueDate !== '') this._dueDate = newDueDate;
+    }
+
+    get status() {
+        return this._status;
+    }
+
+    set status(newStatus) {
+        if (newStatus !== '') this._status = newStatus;
+    }
+    
+};
