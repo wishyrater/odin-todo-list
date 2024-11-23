74fb08f18792060f59c5d81d8228e56802248d19
Julian Andersen <julian.cesar.andersen@gmail.com>
2024-11-23 16:54:45 +0100

    Reset

--- src/index.js
+++ src/index.js
@@ -1,23 +1,4 @@
 import './style.css';
-import { UIRenderer } from './UIRenderer';
-import { ProjectRegistry } from './ProjectRegistry';
-import Project from './Project.js';
-import Task from './Task.js';
-import { EventManager } from './EventManager.js';
 
 document.addEventListener("DOMContentLoaded", () => {
-    const task1 = new Task("One", "Do some stuff", "2024-10-10", "In progress");
-    const task2 = new Task("Two", "More stuff", "2025-01-02", "On hold");
-
-    const myProject = new Project("My project");
-    myProject.addTask(task1);
-    myProject.addTask(task2);
-
-    ProjectRegistry.addProject(myProject);
-
-    UIRenderer.renderProjects(ProjectRegistry.getAllProjects());
-    EventManager.setProjectEvents();
-    EventManager.setTaskEvents();
-    EventManager.setNewProjectEvents();
-
 });
